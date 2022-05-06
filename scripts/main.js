a56c22cdf6ea78cc221e3699c2b9ecb0d0634d38
Streha Aleksandr <Alucard0041@yandex.by>
2022-05-06 14:41:58 +0300

    done task 1

--- scripts/main.js
+++ scripts/main.js
@@ -45,7 +45,7 @@ var ALL_DESCRPTIONS = [
 
 var allImage = [];
 var template = document.querySelector('#picture');
-var pictures = document.querySelector('.pictures');
+var pictures = document.querySelector('.pictures'); 
 
 function getRandomNumber(min, max) {
     return Math.floor(min + Math.random() * (max - min))
@@ -75,16 +75,69 @@ for (var i = 0; i < All_PHOTOS; i++) {
     }
 
     
-    var clone = template.content.cloneNode(true);
-    var image = clone.querySelector('.picture__img');
-    var pictureLikes = clone.querySelector('.picture__likes');
-    var pictureComments = clone.querySelector('.picture__comments');
+    var cloneTemplate = template.content.cloneNode(true);
+    var image = cloneTemplate.querySelector('.picture__img');
+    var pictureLikes = cloneTemplate.querySelector('.picture__likes');
+    var pictureComments = cloneTemplate.querySelector('.picture__comments');
 
     image.src = allImage[i].url;
     pictureLikes.textContent = allImage[i].likes;
     pictureComments.textContent = allImage[i].comments.length;
 
-    pictures.appendChild(clone);
+    pictures.appendChild(cloneTemplate);
 }
 
-console.log(allImage);
+var bigPicture = document.querySelector('.big-picture');
+var allPictures = document.querySelectorAll('.picture');
+var bigViewClose = document.querySelector('#picture-cancel');
+var socialCommentCount = document.querySelector('.social__comment-count');
+var commentLoader = document.querySelector('.comments-loader');
+
+bigViewClose.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    bigPicture.classList.add('hidden');
+})
+
+
+
+function initPicture(index) {
+    function onPictureClick(evt) {
+        evt.preventDefault();
+    
+        var bigView = bigPicture.querySelector('.big-picture__img img');
+        var likeCount = bigPicture.querySelector('.likes-count');
+        var commentsCount = bigPicture.querySelector('.comments-count');
+        var socialCaption = bigPicture.querySelector('.social__caption');
+        var commentList = bigPicture.querySelector('.social__comments');
+
+        while (commentList.firstChild) {
+            commentList.removeChild(commentList.firstChild);
+        } 
+        
+        for (var i = 0; i < allImage[index].comments.length; i++) {
+            var userComment = document.createElement('li');
+            userComment.classList.add('social__comment');
+            userComment.innerHTML = '<img class="social__picture" src="#" alt="Аватар комментатора фотографии" width="35" height="35"> <p class="social__text"></p>';
+
+            userComment.querySelector('img').src = allImage[index].comments[i].avatar;
+            userComment.querySelector('p').textContent = allImage[index].comments[i].message;
+
+            commentList.appendChild(userComment);
+        }
+
+        bigView.src = './' + allImage[index].url;
+        likeCount.textContent = allImage[index].likes;
+        commentsCount.textContent = allImage[index].comments.length;
+        socialCaption.textContent = allImage[index].description;
+    
+        bigPicture.classList.remove('hidden');
+        socialCommentCount.classList.add('visually-hidden');
+        commentLoader.classList.add('visually-hidden');
+    }
+
+    allPictures[i].addEventListener('click', onPictureClick)
+}
+
+for (var i = 0; i < allPictures.length; i++) {
+    initPicture(i);
+}
