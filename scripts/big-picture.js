09e83f51b90b6738648036cf61946888d795bb2a
Streha Aleksandr <Alucard0041@yandex.by>
2022-05-30 15:24:36 +0300

    done task 23

--- scripts/big-picture.js
+++ scripts/big-picture.js
@@ -54,66 +54,58 @@
         } 
     }
 
-    function showBigPicture(pictureData) {   
-        removeComments();
-        
-        var commentView = COMMENT_STEP;
-
+    function viewNewComments(pictureComments, commentView) {
         var commentFragment = document.createDocumentFragment();
-        
-        for (var i = 0; i < Math.min(pictureData.comments.length, COMMENT_STEP); i++) {
+
+        for (var i = commentView; i < Math.min(pictureComments.length, (COMMENT_STEP + commentView)); i++) {
             var userComment = document.createElement('li');
             userComment.classList.add('social__comment');
             userComment.innerHTML = '<img class="social__picture" src="#" alt="Аватар комментатора фотографии" width="35" height="35"> <p class="social__text"></p>';
-
-            userComment.querySelector('img').src = pictureData.comments[i].avatar;
-            userComment.querySelector('p').textContent = pictureData.comments[i].message;
+    
+            userComment.querySelector('img').src = pictureComments[i].avatar;
+            userComment.querySelector('p').textContent = pictureComments[i].message;
 
             commentFragment.appendChild(userComment);
         }
 
         commentList.appendChild(commentFragment);
+    }
+
+    function viewNowComments(commentView, pictureDataCommentLength) {
+        if (commentView < pictureDataCommentLength) {
+            socialCommentCount.firstChild.textContent = commentView + ' из ';
+        }
+        else {
+            socialCommentCount.firstChild.textContent = pictureDataCommentLength + ' из ';
+        } 
+    }
+
+    function showBigPicture(pictureData) {   
+        removeComments();
+
+        var commentView = 0;
+
+        viewNewComments(pictureData.comments, commentView);
+        
+        commentView = COMMENT_STEP;
 
         bigPicture.querySelector('.big-picture__img img').src = pictureData.url;
         bigPicture.querySelector('.likes-count').textContent = pictureData.likes;
         bigPicture.querySelector('.comments-count').textContent = pictureData.comments.length;
         bigPicture.querySelector('.social__caption').textContent = pictureData.description;
 
-        if (commentView < pictureData.comments.length) {
-            socialCommentCount.firstChild.textContent = commentView + ' из ';
-        }
-        else {
-            socialCommentCount.firstChild.textContent = pictureData.comments.length + ' из ';
-        } 
+        viewNowComments(commentView, pictureData.comments.length);
 
         function loadNextComments() {
-            for (var i = commentView; i < Math.min(pictureData.comments.length, (COMMENT_STEP + commentView)); i++) {
-                var userComment = document.createElement('li');
-                userComment.classList.add('social__comment');
-                userComment.innerHTML = '<img class="social__picture" src="#" alt="Аватар комментатора фотографии" width="35" height="35"> <p class="social__text"></p>';
-    
-                userComment.querySelector('img').src = pictureData.comments[i].avatar;
-                userComment.querySelector('p').textContent = pictureData.comments[i].message;
-    
-                commentFragment.appendChild(userComment);
-            }
-    
-            commentList.appendChild(commentFragment);            
-    
-            if ((commentView + COMMENT_STEP) < pictureData.comments.length) {
-                commentView = commentView + COMMENT_STEP;
-            }
-            else {
-                commentView = pictureData.comments.length;
-            }
+            viewNewComments(pictureData.comments, commentView);
+            
+            commentView = commentView + COMMENT_STEP;
 
-            socialCommentCount.firstChild.textContent = commentView + ' из ';
+            viewNowComments(commentView, pictureData.comments.length);
         }
 
-        function commentLoaderDisable() {
-            var countViewComments = document.querySelectorAll('.social__comment');
-    
-            if (countViewComments.length === pictureData.comments.length) {
+        function commentLoaderDisable() {    
+            if (commentView >= pictureData.comments.length) {
                 commentLoader.classList.add('hidden');
             }
         }
